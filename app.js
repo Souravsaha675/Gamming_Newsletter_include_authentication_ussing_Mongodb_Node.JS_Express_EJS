92041bef237e28adb0b5c74e5fe4628ec8be6f86
sourav saha <souravsaha675@gmail.com>
2020-06-05 23:02:12 +0530

    adding video

--- app.js
+++ app.js
@@ -1,68 +1,115 @@
 const express=require("express");
 const bodyparser=require("body-parser");
 const request =require("request");
+const mongoose=require("mongoose");
+const ejs=require("ejs");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 const app=express();
+
+app.set('view engine','ejs');
 
 app.use(express.static("public"));
 
 app.use(bodyparser.urlencoded({extended:true}));
 
-app.get("/",function(req,res){
-    res.sendFile(`${__dirname}/signup.html`)
+
+mongoose.connect("mongodb+srv://admin-souravsaha675:s21072000@cluster0-tvv6o.mongodb.net/gameDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-app.post("/",function(req,res){
-    var firstname =req.body.fname;
-    var lastname = req.body.lname;
-    var email =req.body.email;
+const gameingSchema= new mongoose.Schema({
+    firstname:String,
+    lastname:String,
+    email:String,
+    password:String
+});
 
-    var data={
-        members:[
-            {
-                email_address:email,
-                status: "subscribed",
-                merge_fields:{
-                  FNAME:firstname,
-                  LNAME:lastname,
+const User = new mongoose.model("User",gameingSchema);
+
+app.route("/") 
+    .get(function(req,res){
+        res.render("signup");
+    }) 
+    
+    .post(function(req,res){
+       
+        User.findOne({email:req.body.email},function(err,founduser){
+            if(err){
+                console.log(err);
+            } else{
+                if(!founduser){
+                    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                        const newuser = new User({
+                            firstname: req.body.fname,
+                            lastname: req.body.lname,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        newuser.save(function (err) {
+                            if (!err) {
+                                res.redirect("/index");
+                            } else {
+                                res.redirect("/");
+                            }
+                        });
+                    });
+                   
+                } else{
+                    res.redirect("/login");
                 }
             }
-        ]
-    };
-
-    var jsonData=JSON.stringify(data);  
-
-    var option={
-        url: "https://us18.api.mailchimp.com/3.0/lists/57d26a4c9d",
-        method: "POST",
-        headers:{
-            "Authorization": "sourav 38a556ec481df075398c4c58936fcccd - us18"
-        },
-        body:jsonData
-    }
-
-    request(option,function(error,response ,body){
-        if(error){
-            res.sendFile(__dirname+"/failure.html");
-        }
-        else{
-           if(response.statusCode===200){
-               res.sendFile(__dirname+"/index.html");
-           }
-           else{
-               res.sendFile(`${__dirname}/failure.html`);
-           }
-        }
-
+        });
     });
-    
-});
 
-app.post("/failure",function(req,res){
+app.route("/login")
+    .get(function(req,res){
+        res.render("login");
+    })
 
-    res.redirect("/");
+    .post(function(req,res){
+        const username=req.body.email;
+        const password=req.body.password;
 
-});
+        User.findOne({email:username},function(err,founduser){
+            if(err){
+                console.log(err);
+            } else{
+                if(founduser){
+                    bcrypt.compare(password,founduser.password,function(err,result){
+                        if(result===true){
+                            res.redirect("/index");
+                        }
+                        if(result===false){
+                            res.render("<h1>password is incorret</h1>");
+                        }
+                    });
+                } else{
+                    res.redirect("/signup");
+                }
+            }
+        });
+    });
+
+
+app.route("/index")
+    .get(function(req,res){
+        res.render("index");
+    });
+
+
+app.route("/blog-post")
+    .get(function (req, res) {
+        res.render("blog-post");
+    });
+
+app.route("/logout")
+    .get(function (req, res) {
+        //req.logout();
+        res.redirect("/");
+    });
 
 app.listen(process.env.PORT || 8000,function(){
     console.log("server is running on port 8000.");
